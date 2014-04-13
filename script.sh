a9e335efbfe5ad8a9b4d7688b4c84f2d0699f95c
akshaya <akshayasunny@gmail.com>
2014-04-13 08:59:19 +0530

    Functionality to open the file

--- script.sh
+++ script.sh
@@ -1,9 +1,17 @@
 dir="/etc/tracker";
 
 name=$1;
+file="${dir}/${name}.log";
+
+if [ $arg == "-o" ]
+then
+vim ${file};
+exit 0;
+fi
+
 comment="${*:2}";
 date=`date +%s`;
 
 log_line="${date},${comment}"
 
-echo ${log_line} >> ${dir}/${name}.log;
+echo ${log_line} >> ${file};
